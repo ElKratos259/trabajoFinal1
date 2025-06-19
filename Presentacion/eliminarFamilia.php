240eca5a5007f412fe249af273310a01d63617c0
Piero Mamani Gonza <pieroquispe@gmail.com>
2025-06-19 13:19:31 -0500

    se agrego eliminarFamilia

--- Presentacion/eliminarFamilia.php
+++ Presentacion/eliminarFamilia.php
@@ -1,11 +1,17 @@
 <?php
-require_once '../Logica/LFamilias.php';
+require_once '../Logica/LFamilia.php';
+require_once '../Entidades/Familia.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['idfamilia'])) {
     $id = $_POST['idfamilia'];
+    
+    $familia = new Familia();
+    $familia->setIdfamilia($id);
+
     $log = new LFamilia();
-    $log->eliminar($id);
+    $log->Eliminar($familia);
 }
 
 header('Location: CargarFamilia.php');
 exit;
+?>
